d4a46d0a0d8e96f8fd353e4b0d11e52c645455cc
BlackNoizE404 <43705332+blacknoize404@users.noreply.github.com>
2023-05-03 23:34:11 -0400

    News

---
+++ code.js
@@ -0,0 +1,36 @@
+const nodes = document.querySelectorAll('.grid-item')
+
+function select(element) {
+
+  var div = element.target
+  console.log(div.classList)
+  if (div.classList.contains("selected")) {
+
+    div.classList.remove("selected");
+  }
+  else {
+
+    div.classList.add("selected");
+  }
+  console.log('Hiciste click en el elemento ' + div.innerText);
+}
+
+function unselect_all() {
+  for (let i = 0; i < nodes.length; i++) {
+    var div = nodes[i]
+    if (div.classList.contains("selected")) {
+
+      div.classList.remove("selected");
+    }
+  }
+  console.log('Se han deseleccionado todos');
+}
+
+// Añadiendo evento a todos los elementos
+for (let i = 0; i < nodes.length; i++) {
+  nodes[i].addEventListener('click', select);
+}
+
+var button = document.querySelector("button");
+// Añadiendo evento al botón de reinicio
+button.addEventListener('click', unselect_all)
